f4e94262ee4c6044ca3e83349f6b3a638725e8dc
Satish kumar <satish.kumar@kelltontech.com>
2020-04-08 18:57:14 +0530

    Date

---
+++ Programs/DateMethod.js
@@ -0,0 +1,161 @@
+var date = new Date();
+
+/********************get methods of Date object. ***************/
+/***
+ * Sno :- 1
+ * @Function : DateObject.getDate()
+ * @Desc : Returns numeric day (1 - 31) of the specified date.
+ */
+
+/***
+ * Sno :- 2
+ * @Function : DateObject.getDay()
+ * @Desc : Returns the day of the week (0 - 6) for the specified date.
+ */
+
+/***
+ * Sno :- 3
+ * @Function : DateObject.getFullYear()
+ * @Desc : Returns four digit year of the specified date.
+ */
+
+/***
+ * Sno :- 4
+ * @Function : DateObject.getHours()
+ * @Desc : Returns the hour (0 - 23) in the specified date.
+ */
+
+/***
+ * Sno :- 5
+ * @Function : DateObject.getMilliseconds()
+ * @Desc : Returns the milliseconds (0 - 999) in the specified date.
+ */
+
+/***
+ * Sno :- 6
+ * @Function : DateObject.getMinutes()
+ * @Desc : Returns the minutes (0 - 59) in the specified date.
+ */
+
+/***
+ * Sno :- 7
+ * @Function : DateObject.getMonth()
+ * @Desc : Returns the month (0 - 11) in the specified date.
+ */
+
+/***
+ * Sno :- 8
+ * @Function : DateObject.getSeconds()
+ * @Desc : Returns the seconds (0 - 59) in the specified date.
+ */
+
+/***
+ * Sno :- 9
+ * @Function : DateObject.getTimezoneOffset()
+ * @Desc : It is an inbuilt function of javaScript which is used to return 
+ * the time difference between Universal Co-ordinated Time (UTC) 
+ * and local time, in minutes
+ */
+
+/***
+ * Sno :- 10
+ * @Function : DateObject.getUTCDate()
+ * @Desc : Returns the day (1 - 31) of the month of the specified date as per UTC time zone.
+ */
+
+/***
+ * Sno :- 11
+ * @Function : DateObject.getUTCDay()
+ * @Desc : Returns the day (0 - 6) of the week of the specified date as per UTC timezone.
+ */
+
+/***
+ * Sno :- 12
+ * @Function : DateObject.getUTCFullYear()
+ * @Desc : Returns the four digits year of the specified date as per UTC time zone.
+ */
+
+/***
+ * Sno :- 13
+ * @Function : DateObject.getUTCHours()
+ * @Desc : Returns the hours (0 - 23) of the specified date as per UTC time zone.
+ */
+
+/***
+ * Sno :- 14
+ * @Function : DateObject.getUTCMilliseconds()
+ * @Desc : Returns the milliseconds (0 - 999) of the specified date as per UTC time zone.
+ */
+
+/***
+ * Sno :- 15
+ * @Function : DateObject.getUTCMinutes()
+ * @Desc : Returns the minutes (0 - 59) of the specified date as per UTC time zone.
+ */
+
+/***
+ * Sno :- 16
+ * @Function : DateObject.getUTCMonth()
+ * @Desc : Returns the month (0 - 11) of the specified date as per UTC time zone.
+ */
+
+/***
+ * Sno :- 17
+ * @Function : DateObject.getUTCSeconds()
+ * @Desc : Returns the seconds (0 - 59) of the specified date as per UTC time zone.
+ */
+
+/***
+ * Sno :- 18
+ * @Function : DateObject.getYear()
+ * @Desc : Returns the no of years of the specified date since 1990. This method is Deprecated
+ */
+
+/***
+ * Sno :- 19
+ * @Function : DateObject.toGMTString()
+ * @Desc : Returns a date string in GMT time zone.
+ */
+
+/***
+ * Sno :- 20
+ * @Function : DateObject.toLocaleDateString()
+ * @Desc : Returns the date segment of the specified date using the current locale.
+ */
+
+/***
+ * Sno :- 21
+ * @Function : DateObject.toLocaleFormat()
+ * @Desc : Returns a date string in default format.
+ * @Note : Depricated and can't be used
+ */
+
+/***
+ * Sno :- 22
+ * @Function : DateObject.toLocaleString()
+ * @Desc : Returns a date string using a current locale format.
+ */
+
+/***
+ * Sno :- 23
+ * @Function : DateObject.toLocaleTimeString()
+ * @Desc : Returns the time segment of the specified Date as a string.
+ */
+
+/***
+ * Sno :- 24
+ * @Function : DateObject.toString()
+ * @Desc : Returns a string for the specified Date object.
+ */
+
+/***
+ * Sno :- 25
+ * @Function : DateObject.toUTCString()
+ * @Desc : Returns a string as per UTC time zone.
+ */
+
+ /***
+ * Sno :- 26
+ * @Function : DateObject.valueOf()
+ * @Desc : Returns the primitive value of a Date object.
+ */
